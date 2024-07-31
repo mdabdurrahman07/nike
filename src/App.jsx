b3bbe2a8ff63176330728779848db4df582a9116
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-07-31 22:44:41 +0600

    created some sections and components and applied pre-styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-function App() {
- 
+import Nav from "./components/Nav";
+import CustomerReviews from "./Sections/CustomerReviews";
+import Footer from "./Sections/Footer";
+import Hero from "./Sections/Hero";
+import PopularProducts from "./Sections/PopularProducts";
+import Services from "./Sections/Services";
+import SpecialOffer from "./Sections/SpecialOffer";
+import Subscribe from "./Sections/Subscribe";
+import SuperQuality from "./Sections/SuperQuality";
 
-  return (
-    <div>
-      <h1 className='font-serif text-center font-semibold'>Hello world</h1>
-    </div>
-  )
-}
+const App = () => (
+  <main className="relative">
+    <Nav/>
+
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero/>
+    </section>
+
+    <section className="padding">
+    <PopularProducts/>
+    </section>
+    <section className="padding">
+    <SuperQuality/>
+    </section>
+    <section className="padding-x py-10">
+    <Services/>
+    </section>
+    <section className="padding">
+    <SpecialOffer/>
+    </section>
+    <section className="bg-pale-blue padding">
+    <CustomerReviews/>
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+    <Subscribe/>
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+    <Footer/>
+    </section>
+  </main>
+);
 
 export default App
