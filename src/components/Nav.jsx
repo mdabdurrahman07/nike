12bc9df2d8f50bf57d1277976d989bada9174317
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-07-31 23:18:01 +0600

    nav section created responsive but not functional

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,28 @@
+import { hamburger } from "../assets/icons";
+import headerLogo from "../assets/images/header-logo.svg";
+import { navLinks } from "../constant";
 const Nav = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Nike_logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-start gap-16 max-lg:hidden">
+          {navLinks.map((items) => (
+            <li key={items.label}>
+              <a href={items.href} className="font-montserrat leading-normal text-slate-gray text-lg">
+                {items.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+            <img src={hamburger} alt="nike-mobile-nav-icon" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
